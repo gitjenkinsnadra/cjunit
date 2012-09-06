13a2cd0b6e84acd3db48035f7e29ef4a3851e183
poc_jenkins <some@email.com>
2012-09-06 16:20:31 +0200

    .

---
+++ nminunit_poc/nminunit/nminunit.h
@@ -0,0 +1,17 @@
+#ifndef __NMINUNIT__
+#define __NMINUNIT__ 
+ 
+ 
+ #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+ 
+  
+ #define mu_run_test(test) do { const char *message = test(); tests_run++;  \
+ if (message) return message; } while (0)
+   
+   
+ extern int tests_run;
+ 
+ 
+ 
+ 
+ #endif /* __NMINUNIT__  */
